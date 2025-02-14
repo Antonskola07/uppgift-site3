ae04832d33625229798a32aa5259525dba353a99
Antonskola07 <ihgfeuihfwerhuowerhowerhuo@jiuytyuhij.se>
2025-02-14 11:30:22 +0100

    Börja med flappy

--- cmd.sql
+++ cmd.sql
@@ -1,1 +1,5 @@
 INSERT INTO scores (name, score) VALUES ('Daniel', -1);
+INSERT INTO scores (name, score) VALUES ('Anton', 87654);
+INSERT INTO scores (name, score) VALUES ('Noel', 5);
+INSERT INTO scores (name, score) VALUES ('Alvin', 3);
+INSERT INTO scores (name, score) VALUES ('Albin', 4);
